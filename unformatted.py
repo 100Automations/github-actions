bd2a67dbcbdfe8b93bd56bc6a7c824a8649d8e6d
jafow <jared.a.fowler@gmail.com>
2020-07-21 22:30:33 -0700

    adds unformatted script

---
+++ unformatted.py
@@ -0,0 +1,10 @@
+""" script to demo bad formatting fixed by an action """
+
+
+def my_function():
+    # the formatter doesn't like single-quotes
+    a = 'please format me'
+
+    # the formatter doesn't like long lines > 120 chars
+    long_line = {'pretty_long_key_name': 'this is a very very very very very very vvery very very very very very very very very long value'}
+    return 😊
